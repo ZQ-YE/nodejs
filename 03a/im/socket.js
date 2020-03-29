d873416739fb43dafbec46eead0ada25373f9bd3
YEZQ <497070595@qq.com>
2020-03-29 16:37:29 +0800

    图片上传、表单提交

--- 03a/im/socket.js
+++ 03a/im/socket.js
@@ -2,13 +2,13 @@ const net = require("net");
 const chatServer = net.createServer();
 const clientList = [];
 chatServer.on("connection", client => {
-  client.write("Hi yee\n");
-  clientList.push(client);
-  client.on("data", data => {
-    console.log("receive:", data.toString());
-    clientList.forEach(v => {
-      v.write(data);
+    client.write("Hi yee\n");
+    clientList.push(client);
+    client.on("data", data => {
+        console.log("receive:", data.toString());
+        clientList.forEach(v => {
+            v.write(data);
+        });
     });
-  });
 });
 chatServer.listen(9000);
